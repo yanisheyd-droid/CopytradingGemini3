d5118ca422af8b03f28e7ec748d0e96fdddfdd78
yanisheyd-droid <yanisheyd@gmail.com>
2025-12-16 23:53:09 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { config, validateConfig, runtimeConfig } from './config/environment';
+import { listener } from './core/listener';
+import { copyEngine } from './core/copyEngine'; // Vérifiez la présence de guillemets droits
+import { discoveryWallet } from './core/discoveryWallet';
+import { telegramBot } from './telegram/bot';
+import { ledger } from './core/ledger'; // Vérifiez la présence de guillemets droits
+// ... (reste du fichier)
